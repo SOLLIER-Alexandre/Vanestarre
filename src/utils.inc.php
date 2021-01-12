1b4144ed804e05dcbe9c85b0750a7b5c1ea2cfe4
SOLLIER-Alexandre <alexandre-sollier@hotmail.fr>
2021-01-12 07:24:13 +0100

    utils.inc.php: Documented functions, added search box

--- src/utils.inc.php
+++ src/utils.inc.php
@@ -1,6 +1,14 @@
 <?php
+    /**
+     * Echoes the start of a regular HTML document
+     *
+     * @param string $title The title of the page
+     * @param array $stylesheets Array of stylesheets to include in the page
+     * @param array $scripts Array of scripts to include in the page
+     */
     function start_page($title = 'Vanestarre', $stylesheets = [], $scripts = [])
     {
+        // Start of the <head>
         echo <<<EOL
         <!doctype html>
         <html lang="en">
@@ -16,23 +24,33 @@
 
         EOL;
 
+        // Echo all the stylesheets to the document
         foreach ($stylesheets as $stylesheet) {
             echo '<link rel="stylesheet" href="' . $stylesheet . '">';
         }
 
+        // Echo all the scripts to the document
         foreach ($scripts as $script) {
             echo '<script src="' . $script . '" async></script>';
         }
 
+        // End of the <head>, begin a <body>
         echo <<<EOL
         </head>
         
         <body>
 
         EOL;
-    };
+    }
 
-    function end_page() {
+    ;
+
+    /**
+     * Echoes the end of a regular HTML document (started with the start_page() function)
+     */
+    function end_page()
+    {
+        // End the <body> and <html> tags
         echo <<<'EOL'
         </body>
         
@@ -40,10 +58,20 @@
         EOL;
     }
 
-    function start_layout() {
+    /**
+     * Echoes the start of the standard project layout
+     * This contains the page header with the website title, a search box and an account manager
+     */
+    function start_layout()
+    {
+        // Add the standard <header>, and begin a <main> block
         echo <<<'EOL'
             <header>
                 <a href="/" class="title">Vanestarre</a>
+                <div class="header_right_content">
+                    <input class="search_box" type="search" placeholder="Recherche...">
+                    <span class="material-icons unselectable text_button">search</span>
+                </div>
             </header>
             
             <main>
@@ -51,10 +79,16 @@
         EOL;
     }
 
-    function end_layout() {
+    /**
+     * Echoes the end of the standard project layout (started with the start_layout() function)
+     */
+    function end_layout()
+    {
+        // End the <main> block
         echo <<<'EOL'
             </main>
 
         EOL;
     }
+
 ?>
