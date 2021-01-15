81bfa37c32b7eb1c69fc9050bf950101841899be
EnUtvikler <59125141+EnUtvikler@users.noreply.github.com>
2021-01-15 16:53:17 +0100

    Mise en place de la page de création de compte

--- src/view/create_account_view.php
+++ src/view/create_account_view.php
@@ -14,7 +14,23 @@ class CreateAccountView implements IView
      * @inheritDoc
      */
     public function echo_contents() {
-        echo '        <h1>Template Vanéstarre</h1>' . PHP_EOL;
+        echo <<<'HTML'
+                <div class="create-account-box">
+                    <h1>Créez un compte</h1>
+                
+                    <p>Identifiant :</p>
+                    <input type="text" name="username" class="input-zone" style="color: black"/><br/>
+                    
+                    <p>Email :</p>
+                    <input type="text" name="email" class="input-zone" style="color: black"/><br/>
+                    
+                    <p>Mot de passe :</p>
+                    <input type="password" name="motDePasse" class="input-zone" style="color: black"/><br/>
+                    
+                    <input type="submit" name="Envoyer" class="submit-button" value="Créer votre compte">
+                <div/>
+        HTML;
+
     }
 }
 ?>
