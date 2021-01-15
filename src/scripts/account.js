3d4278980462bbe8bf34289c619e60e370ffde4d
RaspDonut <adrien.chateaux@etu.univ-amu.fr>
2021-01-15 15:14:44 +0100

    Salut les amis, c'est moi Mickey

--- src/scripts/account.js
+++ src/scripts/account.js
@@ -9,8 +9,11 @@
     {
         document.getElementById('show_form_button').onclick = () =>
         {
-            document.getElementById('form_modif_pwd').style.visibility = 'visible';
-            document.getElementById('show_form_button').style.visibility = 'hidden';
+            document.getElementById('pwd_change_frame').style.visibility = 'visible';
+            var pwd_change_frame = document.getElementById('pwd_change_frame');
+            document.getElementById('pwd_change_frame').remove()
+            document.getElementById('main_frame').appendChild(pwd_change_frame);
+            document.getElementById('show_form_button').remove();
         };
     };
 })();
