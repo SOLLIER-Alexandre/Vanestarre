d29f48f20929ce115b4c4c868a2572cb61784125
Cyani-Maisou <tsuki.cookies@gmail.com>
2021-01-18 17:15:26 +0100

    implémentation de add_user()

--- src/Model/AuthDB.php
+++ src/Model/AuthDB.php
@@ -1,5 +1,6 @@
 <?php
-    class AuthDB{
+
+class AuthDB{
         /**
          * @var mysqli $mysqli A mysqli connection to the database.
          */
@@ -20,6 +21,23 @@
          */
         public function __destruct(){
             $this->mysqli->close();
+        }
+
+        /**
+         * @param string $pseudo
+         * @param string $email
+         * @param string $mot_de_passe
+         * Create a new user in the database.
+         */
+        public function add_user(string $pseudo, string $email, string $mot_de_passe){
+            $connection = $this->mysqli;
+            $prepared_query = $connection->prepare('INSERT INTO UTILISATEURS(date_inscription, email, mot_de_passe, pseudo) VALUES (NOW(),?,?,?)');
+            $prepared_query->bind_param('sss', $email, $mot_de_passe, $pseudo);
+            $prepared_query->execute();
+            $result = $prepared_query->get_result();
+            if ($result == false) {
+                throw new Exception("The user couldn't be inserted in the table.");
+            }
         }
     }
 
