596a1c9616e245d7fcdd76fddcf1132887f501c1
EnUtvikler <59125141+EnUtvikler@users.noreply.github.com>
2021-01-24 18:22:09 +0100

    Création de UserPasswordForgotten controller et view

--- src/index.php
+++ src/index.php
@@ -69,7 +69,7 @@
             break;
 
         case '/user/passwordForgotten':
-            $controller = new Controller\User\UserPasswordForgottenController();
+            $controller = new Controller\PasswordForgottenController();
             break;
 
         case '/config/update':
@@ -86,6 +86,10 @@
 
         case '/account':
             $controller = new Controller\AccountController();
+            break;
+
+        case '/passwordForgottenMessage':
+            $controller = new Controller\User\UserPasswordForgottenController();
             break;
 
         case '/config':
