6bb2b61e87e7c349ebcd2fa45d37257575909acc
Cyani-Maisou <tsuki.cookies@gmail.com>
2021-01-28 11:56:10 +0100

    SearchDB commenté

--- src/Model/SearchDB.php
+++ src/Model/SearchDB.php
@@ -28,8 +28,10 @@
          * @throws DatabaseConnectionException
          */
         public function __construct(){
+            // Create a mysqli connection to the database
             $this->mysqli = new mysqli('mysql-vanestarreiutinfo.alwaysdata.net', '222072',
                 '0fQ12HhzmevY', 'vanestarreiutinfo_maindb');
+            // Throws a DatabaseConnectionException if there is an error when establishing the connection
             if ($this->mysqli->connect_errno) {
                 throw new DatabaseConnectionException("Echec lors de la connexion à la base de données : "
                     . $this->mysqli->connect_error);
@@ -60,26 +62,37 @@
                 throw new DatabaseSelectException();
             }
 
+            // Used for the query, which should look like ' % β $tag % '
+            // '%' means "anything" in SQL, β is the tag symbol, $tag is what is in the search
+            // = "anything with 'β$tag' in the messages contents
             $first_part_of_query_param = '%β';
             $end_of_query_param = '%';
             $query_param = $first_part_of_query_param . $tag . $end_of_query_param;
 
+            // Prepare the template for the SQL query
             $prepared_query = $this->mysqli->prepare('SELECT message_id, date, content, reactions_for_donations, image_link ' .
                 'FROM MESSAGES WHERE content LIKE ? ORDER BY date DESC LIMIT ? OFFSET ?');
+            // Bind parameters to the query's template : create a real query
             $prepared_query->bind_param('sii', $query_param, $limit, $offset);
+
             $prepared_query->execute();
 
+            // Stores the query result
             $result = $prepared_query->get_result();
 
+            // If the query result is empty
             if (!$result) {
                 throw new DatabaseSelectException();
             } else {
+                // Create an array to store Message objects
                 $messages_list = array();
 
                 while ($row = $result->fetch_assoc()) {
+                    // Get the number of reactions per type on this message
                     $message_reactions = new MessageReactions();
                     $message_db->get_message_reaction_count($row['message_id'], $message_reactions);
 
+                    // Fills the array with Message objects
                     try {
                         array_push($messages_list, new Message($row['message_id'], $row['content'],
                             new DateTimeImmutable($row['date']), $row['reactions_for_donations'],
@@ -101,19 +114,28 @@
          * @throws DatabaseSelectException
          */
         public function count_messages_with_tag(string $tag): int{
+            // Used for the query, which should look like ' % β $tag % '
+            // '%' means "anything" in SQL, β is the tag symbol, $tag is what is in the search
+            // = "anything with 'β$tag' in the messages contents
             $first_part_of_query_param = '%β';
             $end_of_query_param = '%';
             $query_param = $first_part_of_query_param . $tag . $end_of_query_param;
 
+            // Prepare the template for the SQL query
             $prepared_query = $this->mysqli->prepare('SELECT count(*) FROM MESSAGES WHERE content LIKE ? ');
+            // Bind parameters to the query's template : create a real query
             $prepared_query->bind_param('s', $query_param);
+
             $prepared_query->execute();
 
+            // Stores the query result
             $result = $prepared_query->get_result();
 
+            // If the query result is empty
             if (!$result) {
                 throw new DatabaseSelectException();
             } else {
+                // Return the number of messages with the tag given as parameter
                 $row = $result->fetch_assoc();
                 return $row['count(*)'];
             }
